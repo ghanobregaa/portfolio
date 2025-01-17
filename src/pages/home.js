81dbd877ca9846b5320d15942109a2963c3a3f3a
Guilherme Nobrega <ghanobrega@gmail.com>
2025-01-17 16:52:17 +0000

    first commit

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.column}>
+        <h1>SOFTWARE</h1>
+        <h2>DEVELOPER</h2>
+      </div>
+    </div>
+  );
+}
