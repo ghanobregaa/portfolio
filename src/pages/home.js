db2c935f0dee406ceaf6397f0bb4304a6edc774b
Guilherme Nóbrega <ghanobrega@gmail.com>
2025-01-18 22:35:40 +0000

    update

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,9 +4,16 @@ import styles from "./styles.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={styles.column}>
-        <h1>SOFTWARE</h1>
-        <h2>DEVELOPER</h2>
+      <div className={styles.row}>
+        <div className={styles.card}>
+          <div className={styles.picture}>
+            <img src="Profile.jpeg" />
+          </div>
+        </div>
+        <div className={`${styles.column} ${styles.title}`}>
+          <span>Software</span>
+          <span>Developer</span>
+        </div>
       </div>
     </div>
   );
