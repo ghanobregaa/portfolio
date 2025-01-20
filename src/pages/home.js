3f52c7d4e3de337a760f270a5453ff21117b63bb
Guilherme Nobrega <ghanobrega@gmail.com>
2025-01-20 17:55:33 +0000

    Update

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,22 @@
 import React from "react";
 import styles from "./styles.module.css";
+import { ReactComponent as HomeSvg } from "../icons/home_white.svg";
+import { ReactComponent as FileSvg } from "../icons/file_white.svg";
+import { ReactComponent as WrenchSvg } from "../icons/wrench_white.svg";
+import { ReactComponent as WorkSvg } from "../icons/work_white.svg";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={styles.row}>
-        <div className={styles.card}>
-          <div className={styles.picture}>
-            <img src="Profile.jpeg" />
+      <div className={styles.banner}>
+        <div className={styles.centerAvatar}>
+          <div className={styles.avatar}>
+            <img src="Picture.jpeg" />
           </div>
         </div>
-        <div className={`${styles.column} ${styles.title}`}>
-          <span>Software</span>
-          <span>Developer</span>
-        </div>
+      </div>
+      <div className={styles.rowContent}>
+        <div className={styles.btnWatch}>Download CV</div>
       </div>
     </div>
   );
