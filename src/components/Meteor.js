ed22a47c0e3a4c0d80db83346e7153dd10dc669b
Guilherme Nóbrega <ghanobrega@gmail.com>
2025-01-23 22:14:36 +0000

    update

--- src/components/Meteor.js
+++ src/components/Meteor.js
@@ -4,14 +4,26 @@ import styles from "./Meteor.module.css";
 export default function Meteor() {
   return (
     <div className={styles.container}>
-      {Array.from({ length: 20 }).map((_, index) => (
+      {Array.from({ length: 14 }).map((_, index) => (
         <div
           key={index}
           className={styles.meteor}
           style={{
-            left: `${-30 + index * 10}%`,
+            left: `${-25 + index * 10}%`,
             animationDelay: `${Math.random() * index}s`,
-            animationDuration: `${(Math.random() * (1 - 0.5) + 0.5) * 12}s`,
+            animationDuration: `${(Math.random() * (1 - 0.5) + 0.5) * 8}s`,
+            animationComposition: "replace",
+          }}
+        ></div>
+      ))}
+      {Array.from({ length: 14 }).map((_, index) => (
+        <div
+          key={index}
+          className={styles.meteor}
+          style={{
+            left: `${-40 + index * 10}%`,
+            animationDelay: `${Math.random() * index}s`,
+            animationDuration: `${(Math.random() * (1 - 0.5) + 0.5) * 4}s`,
             animationComposition: "replace",
           }}
         ></div>
